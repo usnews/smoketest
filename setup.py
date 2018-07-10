b02a6bf6ba4a28b6b65ea4fa3af99b2271b02f03
Byron Peebles <byron.peebles@gmail.com>
2018-07-10 11:42:36 -0400

    setup: Add 3.7 to PyPi classifiers

--- setup.py
+++ setup.py
@@ -42,6 +42,7 @@ config = {
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Internet',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Software Development',
